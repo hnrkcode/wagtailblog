053c35fda25d4a62d60d9d01ff73e4480f2e94c0
Henrik Petersson <henrik@tutamail.com>
2019-05-14 13:50:20 +0200

    added local.py to prepare for production

---
+++ wagtailblog/settings/local.py
@@ -0,0 +1,7 @@
+import os
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*', ]
